caa68fd1a40f3d4f952366e40623835de444a812
KYurtseven <koray.can.yurtseven@gmail.com>
2018-07-09 15:29:23 +0300

    date and deposit
    Signed-off-by: KYurtseven <koray.can.yurtseven@gmail.com>

--- react_v1/src/Home.js
+++ react_v1/src/Home.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import tubitak_logo from './img/tubitak-logo.jpg';
-import './App.css';
+import './Home.css';
 import * as Constants from './Constants' ;
 import * as BasePage from './BasePage';
+import UserItem from './UserItem';
 
 class Home extends Component
 {
@@ -11,27 +12,42 @@ class Home extends Component
         super(props);
         this.state = 
         {
+            // initially load the data
             isLoading: true,
-            userData : {}
+            // when the submit button is pressed
+            isSubmit: false,
+            userData : {},
+            submitValue : []
         }
+    }
+
+    sortByKey(array, key)
+    {
+        return array.sort(function(a,b){
+            var x = a[key]; var y = b[key];
+            return((x < y) ? -1: ((x > y) ? 1 : 0));
+        });
     }
 
     async componentDidMount()
     {
         if(Constants.IS_MOCK)
         {
+            var tmp = Constants.MOCK_USER_OBJ;
+
+            this.setState({isLoading: false, userData: tmp});
+        }
+        else
+        {
             var url;
             url = Constants.getRoot() + Constants.exampleURL;
 
             let res = await BasePage.CallApiGet(url);
             let restext = await res.text();
+            let resJSON = JSON.parse(restext);
 
-            this.setState({isLoading: false, userData: JSON.parse(restext)});
-            
-        }
-        else
-        {
-            // TO DO
+            resJSON = this.sortByKey(resJSON, 'username');
+            this.setState({isLoading: false, userData: resJSON});
         }
     }
 
@@ -50,11 +66,11 @@ class Home extends Component
             var renderCassandra = []
             for(let i = 0; i < this.state.userData.length; i++)
             {
-                console.log(this.state.userData[i].username)
                 renderCassandra.push(
                     <UserItem
                         key = {i}
-                        userInfo = {this.state.userData[i]}/>
+                        userInfo = {this.state.userData[i]}
+                    />
                 );
             }
 
@@ -65,7 +81,6 @@ class Home extends Component
                         <h1 className = "App-title" style = {styles.tubitak_header_text}> TÜBİTAK </h1>
                     </header>
                     {renderCassandra}
-                    <EssayForm/>
                 </div>
             );
         }
@@ -90,97 +105,5 @@ const styles =
         height: 100
     }
 }
-
-const UserItem = (props) => 
-{
-    var lateDates = [];
-    
-    if(props.userInfo.dates == null || props.userInfo.dates == undefined)
-        return(
-            <div className = "User-div-container">
-                <div className = "User-div-left">
-                    <p className= "User-text-left">username: {props.userInfo.username}</p>
-                    <p className= "User-text-left">email: {props.userInfo.email}</p>
-                    <p className= "User-text-left">name: {props.userInfo.name}</p>
-                    <p className= "User-text-left">deposit: {props.userInfo.deposit}</p>
-                    <p className= "User-text-left">userType: {props.userInfo.usertype}</p>
-                </div>
-                <div className="User-div-right">
-                    <p className= "User-text-right">No Late Data</p>
-                    <input type="submit" value="Submit" />
-                </div>
-            </div>
-        );
-
-    for(var i = 0; i < props.userInfo.dates.length; i++)
-    {
-        lateDates.push(
-            <LateDates 
-                key = {i}
-                date = {props.userInfo.dates[i]}/>
-        );
-    }
-    return(
-        <div className= "User-div-container">
-            <div className = "User-div-left" >
-                <p className= "User-text-left">username: {props.userInfo.username}</p>
-                <p className= "User-text-left">email: {props.userInfo.email}</p>
-                <p className= "User-text-left">name: {props.userInfo.name}</p>
-                <p className= "User-text-left">deposit: {props.userInfo.deposit}</p>
-                <p className= "User-text-left">userType: {props.userInfo.usertype}</p>
-            </div>
-            <div className = "User-div-right">
-                {lateDates}
-                <form>
-                    <label>
-                        Name:
-                    <input type="text" name="name" />
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
-            </div>
-        </div>
-    );
-}
-
-const LateDates = (props) =>
-{
-    return(
-        <li className= "User-text-right">{props.date}</li>
-    );
-}
-
-class EssayForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        value: 'Please write an essay about your favorite DOM element.'
-      };
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
-  
-    handleSubmit(event) {
-      alert('An essay was submitted: ' + this.state.value);
-      event.preventDefault();
-    }
-  
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Essay:
-            <textarea value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      );
-    }
-  }
 
 export default Home;
--- react_v1/src/index.js
+++ react_v1/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Home from './Home';
 
+class App extends React.Component {
+    render()
+    {
+      return(<Home />);
+    }
+  }
+  
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
