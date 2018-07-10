addb038aef10a3de18873a5d0b3b367ef9afe696
KYurtseven <koray.can.yurtseven@gmail.com>
2018-07-10 11:25:33 +0300

    bootstrap integratio
    Signed-off-by: KYurtseven <koray.can.yurtseven@gmail.com>

--- react_v1/src/UserItem.js
+++ react_v1/src/UserItem.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import * as Constants from './Constants' ;
 import * as BasePage from './BasePage';
 import './Home.css';
+import {
+    Button, 
+    Modal,
+    FormGroup,
+    ControlLabel,
+    FormControl
+} from 'react-bootstrap';
+
 
 class UserItem extends Component 
 {
@@ -10,7 +18,9 @@ class UserItem extends Component
         super(props);
         this.state = {
             dateValue : '',
-            depositValue: ''
+            depositValue: '',
+            isShowDateModal: false,
+            isShowDepositModal: false
         };
 
         this.handleDateChange = this.handleDateChange.bind(this);
@@ -18,20 +28,40 @@ class UserItem extends Component
 
         this.handleDepositChange = this.handleDepositChange.bind(this);
         this.handleDepositSubmit = this.handleDepositSubmit.bind(this);
+   
+        this.showDateModal = this.showDateModal.bind(this);
+        this.hideDateModal = this.hideDateModal.bind(this);
+
+        this.showDepositModal = this.showDepositModal.bind(this);
+        this.hideDepositModal = this.hideDepositModal.bind(this);
+
     }
 
+    // DATE FUNCTIONS
+
+    // sets state of the date entry
     handleDateChange(event)
     {
         this.setState({dateValue: event.target.value});
     }
 
+    // post to the database
     handleDateSubmit(event)
     {
         if(BasePage.isValidDate(this.state.dateValue))
         {
-            this.addDate(this.state.dateValue, this.props.userInfo.username);
+            var url;
+            url = Constants.getRoot() + Constants.addDate;
+
+            let body = JSON.stringify({
+                username : this.props.userInfo.username,
+                date : this.state.dateValue
+            });
+            BasePage.CallApiPost(url, body);
+
             // TO DO
             event.preventDefault(); // how it works? What is it?
+            this.setState({isShowDateModal : false});
         }
         else
         {
@@ -39,16 +69,64 @@ class UserItem extends Component
         }
     }
 
+    showDateModal()
+    {
+        this.setState({isShowDateModal: true});
+    }
+
+    hideDateModal() {
+        this.setState({isShowDateModal: false});
+    }
+
+    renderDateModal()
+    {
+        return(
+            <Modal show={this.state.isShowDateModal} onHide={this.hideDateModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add Late Date</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form>
+                        <FormGroup
+                        controlId = "formBasicText"
+                        validationState = {BasePage.isValidDate(this.state.dateValue)}
+                        >
+                        <ControlLabel>Enter late date, format is yyyy-MM-dd</ControlLabel>
+                        <FormControl
+                            type="text"
+                            value={this.state.dateValue}
+                            placeholder="Enter text"
+                            onChange={this.handleDateChange}
+                        />
+                        <FormControl.Feedback />
+                        </FormGroup>
+                    </form>
+
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick = {this.hideDateModal}>Close</Button>
+                    <Button onClick = {this.handleDateSubmit} bsStyle="success">ADD</Button>
+                </Modal.Footer>
+            </Modal>
+        );
+    }
+
+    // END OF DATE FUNCTIONS
+    
+    
+    // DEPOSIT FUNCTIONS
+
+    // sets state of the deposit entry
     handleDepositChange(event)
     {
         this.setState({depositValue: event.target.value});
     }
 
+    // post to the database
     handleDepositSubmit(event)
     {
         if(!isNaN(this.state.depositValue))
         {
-            alert("Entered value was: " + this.state.depositValue);
             var url;
             url = Constants.getRoot() + Constants.updateDeposit;
 
@@ -56,25 +134,61 @@ class UserItem extends Component
                 username : this.props.userInfo.username,
                 deposit : this.state.depositValue
             });
-            BasePage.CallApiPost(url, body).done(() => {});
+            //BasePage.CallApiPost(url, body).done(() => {});
+            BasePage.CallApiPost(url, body);
+
+            this.setState({isShowDepositModal : false});
         }
         else
         {
             alert("Please enter an integer");
         }
     }
- 
-    addDate(val, name)
-    {
-        var url;
-        url = Constants.getRoot() + Constants.addDate;
 
-        let body = JSON.stringify({
-            username : name,
-            date : val
-        });
-        BasePage.CallApiPost(url, body).done(() => {});
+    showDepositModal()
+    {
+        this.setState({isShowDepositModal: true});
     }
+
+    hideDepositModal() {
+        this.setState({isShowDepositModal: false});
+    }
+
+    renderDepositModal()
+    {
+        return(
+            <Modal show={this.state.isShowDepositModal} onHide={this.hideDepositModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Update Deposit</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form>
+                        <FormGroup
+                        controlId = "formBasicText"
+                        validationState = {(!isNaN(this.state.depositValue) && this.state.depositValue != '') ? 'success' : 'error'}
+                        >
+                        <ControlLabel>Enter deposit</ControlLabel>
+                        <FormControl
+                            type="text"
+                            value={this.state.depositValue}
+                            placeholder="Enter value"
+                            onChange={this.handleDepositChange}
+                        />
+                        <FormControl.Feedback />
+                        </FormGroup>
+                    </form>
+
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick = {this.hideDepositModal}>Close</Button>
+                    <Button onClick = {this.handleDepositSubmit} bsStyle="success">Update</Button>
+                </Modal.Footer>
+            </Modal>
+        );
+    }
+
+    // END OF DEPOSIT FUNCTIONS
+
 
     renderNoLateData()
     {
@@ -123,8 +237,25 @@ class UserItem extends Component
                 </div>
                 <div className = "User-div-right">
                     {isDataAcceptable ? this.renderLateDates(lateDates) : this.renderNoLateData()}
-                    
-                    <div>
+
+                    <Button bsStyle="primary" bsSize="large" onClick={this.showDateModal}>
+                        Add Late Date
+                    </Button>
+
+                    <Button bsStyle="primary" bsSize="large" onClick={this.showDepositModal}>
+                        Update Deposit
+                    </Button>
+
+                    {this.renderDateModal()}
+                    {this.renderDepositModal()}
+                </div>
+            </div>
+        );
+    }
+}
+
+/**
+ <div>
                         <form onSubmit={this.handleDateSubmit}>
                             <label>
                                 Add Late Date:
@@ -133,24 +264,7 @@ class UserItem extends Component
                             <input type="submit" value="Submit" />
                         </form>
                     </div>
-
-                    <div>
-                        <form onSubmit={this.handleDepositSubmit}>
-                            <label>
-                                Update Deposit:
-                                <textarea value={this.state.depositValue} onChange={this.handleDepositChange} />
-                            </label>
-                            <input type="submit" value="Submit" />
-                        </form>
-                    </div>
-
-                </div>
-            </div>
-        );
-    }
-
-}
-
+ */
 const LateDates = (props) =>
 {
     return(
